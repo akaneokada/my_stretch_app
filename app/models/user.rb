e761710ae815230f082961e4637c8e38437a6577
aknokd <aknokd.814@gmail.com>
2020-10-12 23:58:58 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :gender
+    validates :birthday
+    validates :email,                 uniqueness: { case_sensitive: true },
+                                      format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password,              confirmation: true,
+                                      format: { with: /\A[a-z\d]+\z/i }
+    validates :password_confirmation
+  end
+
 end
